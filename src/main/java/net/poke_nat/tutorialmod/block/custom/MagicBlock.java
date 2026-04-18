cef2fb9e1d84c64a93131353bb6f784f15bd2727
Nat-200606 <nataliamaciel20006@gmail.com>
2026-04-17 21:30:07 -0300

    Added first custom items and recipes

--- src/main/java/net/poke_nat/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/net/poke_nat/tutorialmod/block/custom/MagicBlock.java
@@ -1,20 +1,25 @@
 package net.poke_nat.tutorialmod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.neoforged.neoforge.client.event.sound.SoundEvent;
 import net.poke_nat.tutorialmod.item.ModItems;
+
+import java.util.List;
 
 public class MagicBlock extends Block {
 
@@ -38,5 +43,11 @@ public class MagicBlock extends Block {
         }
 
         super.stepOn(level, pos, state, entity);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        tooltipComponents.add(Component.translatable("tooltip.tutorialmod.magic_block.tooltip"));
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
     }
 }
